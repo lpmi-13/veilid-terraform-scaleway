2a88c375dba022bd1b65cd61a0a3be0db08c1424
Adam Leskis <leskis@gmail.com>
2024-05-07 22:20:43 +0100

    remove unneeded port 5151 access

--- security-groups.tf
+++ security-groups.tf
@@ -26,40 +26,6 @@ resource "scaleway_instance_security_group" "veilid" {
     for_each = local.needIpv4 ? [true] : []
     content {
       action   = "accept"
-      port     = "5151"
-      protocol = "TCP"
-      ip_range = "0.0.0.0/0"
-    }
-  }
-
-  inbound_rule {
-    action   = "accept"
-    port     = "5151"
-    protocol = "TCP"
-    ip_range = "::/0"
-  }
-
-  dynamic "inbound_rule" {
-    for_each = local.needIpv4 ? [true] : []
-    content {
-      action   = "accept"
-      port     = "5151"
-      protocol = "UDP"
-      ip_range = "0.0.0.0/0"
-    }
-  }
-
-  inbound_rule {
-    action   = "accept"
-    port     = "5151"
-    protocol = "UDP"
-    ip_range = "::/0"
-  }
-
-  dynamic "inbound_rule" {
-    for_each = local.needIpv4 ? [true] : []
-    content {
-      action   = "accept"
       port     = "5150"
       protocol = "TCP"
       ip_range = "0.0.0.0/0"
